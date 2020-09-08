695e01e61725f1cecdf38929dd3b600dda9bcb62
Jose Signoret <jsignoretfernandez@gmail.com>
2020-09-08 10:41:29 +0200

    Funcionalidad de busqueda de anuncios

--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const Anuncio = require('../models/Anuncio')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Nodepop' });
+router.get('/', async function(req, res, next) {
+
+  try {
+    const anuncios = await Anuncio.list();
+    res.render('index', { title: 'Nodepop', anuncios: anuncios});
+  }catch (e){
+    next(e);
+  }
+
+
 });
 
 module.exports = router;
