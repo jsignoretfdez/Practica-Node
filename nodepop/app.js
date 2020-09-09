249e968dcb52c29437625cd6c4540f45d2265ff4
Jose Signoret <jsignoretfernandez@gmail.com>
2020-09-09 17:23:51 +0200

    Pequeños cambios

--- nodepop/app.js
+++ nodepop/app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 const mongoose = require('./lib/connectionDB');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const crearRouter = require('./routes/crear-anuncio');
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/crear-anuncio', crearRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
