f36b83617f3be04960ba4350967d32eab7f0d4c2
Jose Signoret <jsignoretfernandez@gmail.com>
2020-09-10 17:06:28 +0200

    Funcionalidad de multer

--- nodepop/routes/apiv1/anuncios.js
+++ nodepop/routes/apiv1/anuncios.js
@@ -1,9 +1,22 @@
 'use strict';
 
 const express = require('express');
+const multer = require('multer');
 
 const router = express.Router();
 const Anuncio = require('../../models/Anuncio');
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, './public/images/anuncios');
+  },
+  filename(req, file, cb) {
+    const myFilename = `${Date.now()}_${file.originalname}`;
+    cb(null, myFilename);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 /* Post  */
 router.post('/crear-anuncio', (req, res, next) => {
@@ -11,12 +24,13 @@ router.post('/crear-anuncio', (req, res, next) => {
 });
 
 /* POST to upload new anuncio */
-router.post('/upload', async (req, res, next) => {
+router.post('/upload', upload.single('foto'), async (req, res, next) => {
   try {
     const {
-      nombre, precio, tags, foto,
+      nombre, precio, tags,
     } = req.body;
     let venta = req.body;
+    const foto = req.file.filename;
     /* Compruebo el select y determino si es verdadero o falso */
 
     if (venta === 'venta') {
