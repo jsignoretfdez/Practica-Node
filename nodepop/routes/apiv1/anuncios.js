0138293f01b53d8fb954ce783e0c12f00886afb2
Jose Signoret <jsignoretfernandez@gmail.com>
2020-09-10 15:53:22 +0200

    Refactor

--- nodepop/routes/apiv1/anuncios.js
+++ nodepop/routes/apiv1/anuncios.js
@@ -1,47 +1,42 @@
-var express = require('express');
-var router = express.Router();
-var Anuncio = require('../../models/Anuncio');
+'use strict';
+
+const express = require('express');
+
+const router = express.Router();
+const Anuncio = require('../../models/Anuncio');
 
 /* Post  */
-router.post('/crear-anuncio', function(req, res, next) {
-
-    res.render('crear');
-
+router.post('/crear-anuncio', (req, res, next) => {
+  res.render('crear');
 });
 
 /* POST to upload new anuncio */
-router.post('/upload', async function(req, res, next) {
-    try {
-        let {nombre, precio, venta, tags, foto} = req.body;
-        /*const precio = req.body.precio;
-        const venta = req.body.venta;
-        const tags = req.body.tags;
-        const foto = req.body.foto*/
+router.post('/upload', async (req, res, next) => {
+  try {
+    const {
+      nombre, precio, tags, foto,
+    } = req.body;
+    let venta = req.body;
+    /* Compruebo el select y determino si es verdadero o falso */
 
-        /* Compruebo el select y determino si es verdadero o falso */
-
-        if(venta === 'venta'){
-            venta = true
-
-        }else{
-            venta = false;
-        }
-
-        // Creamos el documento en memoria
-        const anuncio = new Anuncio({nombre, precio, venta, tags, foto});
-
-        // Lo guardamos en BD
-        await anuncio.save();
-
-       res.redirect('/');
-
-    } catch (err){
-        next(err);
+    if (venta === 'venta') {
+      venta = true;
+    } else {
+      venta = false;
     }
+
+    // Creamos el documento en memoria
+    const anuncio = new Anuncio({
+      nombre, precio, venta, tags, foto,
+    });
+
+    // Lo guardamos en BD
+    await anuncio.save();
+
+    res.redirect('/');
+  } catch (err) {
+    next(err);
+  }
 });
-
-
-
-
 
 module.exports = router;
