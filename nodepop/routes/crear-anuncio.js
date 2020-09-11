23c4fa012a51d1c70de50821df80153588ca4d02
Jose Signoret <jsignoretfernandez@gmail.com>
2020-09-11 23:30:29 +0200

    Refactor

--- nodepop/routes/crear-anuncio.js
+++ nodepop/routes/crear-anuncio.js
@@ -1,11 +1,13 @@
-var express = require('express');
-var router = express.Router();
+/* eslint-disable strict,lines-around-directive */
+'use strict';
+
+const express = require('express');
+
+const router = express.Router();
 
 /* GET home page. */
-router.post('/', function(req, res, next) {
-
-res.render('crear');
-
+router.post('/', (req, res, next) => {
+  res.render('crear');
 });
 
 module.exports = router;
