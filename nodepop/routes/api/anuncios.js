86d43a36d91a64dd21da33c845d0811a07ec9f2c
Jose Signoret <jsignoretfernandez@gmail.com>
2020-09-11 22:41:54 +0200

    Filtro venta, tags, precio

--- nodepop/routes/api/anuncios.js
+++ nodepop/routes/api/anuncios.js
@@ -53,15 +53,44 @@ router.post('/upload', upload.single('foto'), async (req, res, next) => {
 
 router.get('/', async (req, res, next) => {
   try {
-    const { nombre, sort } = req.query;
+    const {
+      nombre, sort, tags, precio, venta,
+    } = req.query;
     const limit = parseInt(req.query.limit || 10);
     const skip = parseInt(req.query.skip);
     const filtro = {};
-    if (nombre) {
-      filtro.nombre = nombre;
+    if (nombre !== undefined) {
+      filtro.nombre = new RegExp(`^${
+        nombre}`, 'i');
     }
 
-    const anuncios = await Anuncio.list(filtro, limit, skip, sort);
+    // Filtro Rango de precios
+    if (precio !== undefined) {
+      const precioSave = precio.split('-').map(parseFloat);
+      if (precioSave.length >= 1) {
+        console.log(precioSave[0], precioSave[1]);
+        filtro.precio = precio;
+        if (precioSave[0] && !precioSave[1]) {
+          console.log('Hola');
+          filtro.precio = { $gte: precioSave[0] };
+        } if (!precioSave[0] && precioSave[1]) {
+          filtro.precio = { $lte: precioSave[1] };
+        } if (precioSave[0] && precioSave[1]) {
+          filtro.precio = { $gte: precioSave[0], $lte: precioSave[1] };
+        }
+      }
+    }
+    // Filtro Tags
+    if (typeof tags !== 'undefined') {
+      // Convert tags string into array
+      filtro.tags = { $in: tags.split(' ') };
+    }
+    // Filtro Venta
+    if (venta !== undefined) {
+      filtro.venta = venta;
+    }
+    // Listado de anuncios
+    const anuncios = await Anuncio.list(filtro, limit, skip, sort, tags);
     res.render('anuncios', { anuncios });
   } catch (e) {
     next(e);
